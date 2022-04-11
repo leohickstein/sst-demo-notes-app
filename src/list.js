7b86810ca82c46ec2e015f0a678dd56c847ba50f
leohickstein <leonardohickstein@hotmail.com>
2022-04-10 19:11:33 -0700

    Adding the API

---
+++ src/list.js
@@ -0,0 +1,21 @@
+import handler from "./util/handler";
+import dynamoDb from "./util/dynamodb";
+
+export const main = handler(async(event) => {
+    const params = {
+        TableName: process.env.TABLE_NAME,
+        // 'KeyConditionExpression' defines the condition for the query
+        // - 'userId = :userId': only return items with matching 'userId' partition key
+        KeyConditionExpression: "userId = :userId",
+        // 'ExpressionAttributeValues' defines the value in the condition
+        // - ':userId': defines 'userId' to be Identity Pool identity id
+        ExpressionAttributeValues: {
+            ":userId": "123",
+        },
+    };
+
+    const result = await dynamoDb.query(params);
+
+    // Return the retrieved item
+    return result.Items;
+})
